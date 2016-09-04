7b0c027892fa33c606503a1ca28517d9636b29d4
DmitriyMazurov <mdk9@ya.ru>
2016-09-04 17:24:22 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+$(function(){
+
+  $('.tabs .tabs__header a').on('click', function(e){
+    e.preventDefault();
+    var href = $(this).attr('href');
+    $('.tabs .tabs__header a').removeClass('active');
+    $(this).addClass('active');
+    $('.tabs__content div').hide();
+    $(href).fadeIn(700);
+  })
+
+  $('.form .form__input').on('mouseover', function(e){
+    $(this).siblings('.form__tooltip').stop().fadeIn(500);
+  });
+
+  $('.form .form__input').on('mouseout', function(e){
+    $(this).siblings('.form__tooltip').stop().fadeOut(500);
+  });
+
+  $('.form .form__button').on('click', function(e){
+    $('.form .form__tooltip').stop().fadeToggle(500);
+  })
+
+})
